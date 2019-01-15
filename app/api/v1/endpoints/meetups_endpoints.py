a8a8cb95ca1d8f163f425e8bdfb0cafa53fcb677
vmuthabuku <vmuthabuku@gmail.com>
2019-01-15 16:33:06 +0300

    [starts #163224068] Addition of information on readme

--- app/api/v1/endpoints/meetups_endpoints.py
+++ app/api/v1/endpoints/meetups_endpoints.py
@@ -12,65 +12,80 @@ meetups=[]
 rsvp=[]
 
 parser = reqparse.RequestParser()
-parser.add_argument('createdOn', required=True, help="Name cannot be blank!")
-parser.add_argument('location', type=str, required=True, help="price can only be an integer")
-parser.add_argument('topic', type=str, required=True, help="quantity can only be an integer")
-parser.add_argument('happeningOn', required=True, help="Description cannot be blank")
+parser.add_argument('location', type=str, required=True, help="location cannot be blank")
+parser.add_argument('topic', type=str, required=True, help="topic cannot be blank")
+parser.add_argument('happeningOn', required=True, help="happening on cannot be blank")
 parser.add_argument('tags', required=True, help="tags cannot be blank")
 
 class get_meetups(Resource):
     """This handles getting all available meetups"""
     def get(self):
-        """This handles getting all available meetups"""
         return {'data':meetups,
-                'status':200}, 200
-    
+                'status':200}, 200    
 
 
 class get_all(Resource):
 
     """"
-    This class gets all questions and posts a question
+    This handles posting a new meetup
     """
 
     def post(self):
         """This handles posting a meetup"""
         data = parser.parse_args()
+        location = data.get('location')
+        topic = data.get('topic')
+        happeningOn = data.get('happeningOn')
+        tags = data.get('tags')
+
+        for s in (data['location'],data['topic'],data['happeningOn'],data['tags']):            
+            if validator.check_empty(s):
+                return {'error':'value {} cannot be empty'.format(s)}, 400  
+        
+        validate = validator.check_meetup_duplicate(meetups, data['topic'])
+        if validate:
+               return {"message": validate}, 409
+           
 
         id_count = 1
         for meetup in meetups:
             id_count += 1
 
-        new_item = Meetup(data['createdOn'], data['location'], data['topic'], data['happeningOn'],data['tags'])
+        new_item = Meetup(data['location'], data['topic'], data['happeningOn'],data['tags'])
         new_item_dict = new_item.make_dict(id_count)
-
         meetups.append(new_item_dict)
         return {'message': 'Your item has been added successfully',
                 'data':new_item_dict,
                 'status':201}, 201 
+        
 class get_specific(Resource):
-    """Get a specific question"""
+    """Get a specific meetup"""
     @classmethod
     def get(cls, meetupid):
-        check_id = validator.check_using_id(meetups,int(meetupid))
+        check_id = validator.check_id(meetups,int(meetupid))
         if check_id:
             return check_id, 200
-        return {'message':'no such id'}
+        return {'error':'the id {} does not exist'.format(meetupid)}, 400
 
 class Rsv(Resource):
+    """This handles to rsvp for a specific meetup"""
     parser = reqparse.RequestParser()
-    parser.add_argument('status')
-    parser.add_argument('topic')
+    parser.add_argument('status',required=True, help="status cannot be empty")
     def post(self,meetupid):
         data = Rsv.parser.parse_args()
-        new = Rsvp(data['status'],data['topic'])
-        new_item = new.make_dic()
-        rsvp.append(new_item)
-        return{'status':rsvp,
-               'meetupid':meetupid},201
+        status = data.get('status')
+        check_id = validator.check_id(meetups,int(meetupid))
+        if not check_id:
+            return {'error':'the id {} does not exist'.format(meetupid)}, 400
+        if status == "yes" or status == "no" or status ==  "maybe":
+            new = Rsvp(data['status'])
+            new_item = new.make_dic()
+            rsvp.append(new_item)
+            return{'status':new_item,
+                'meetupid':meetupid},201
+        return {"message":"There is no such status {}".format(status)}, 400                 
 
-
-
+        
 
 api.add_resource(get_all, "/meetups")
 api.add_resource(get_meetups, "/meetups/upcoming")
